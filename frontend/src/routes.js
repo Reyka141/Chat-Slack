abfb3c65224fc6dd5d0a9df82122f7a804941369
Andrii <youdronveduta@gmail.com>
2024-09-22 14:34:31 +0200

    update frontend/src/routes.js

---
+++ frontend/src/routes.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+const apiPath = '/api/v1';
+
+export default {
+  loginPath: () => [apiPath, 'login'].join('/'),
+  usersPath: () => [apiPath, 'data'].join('/'),
+};
