cd56424f46e53abfa1c15817594b2c3826ed970d
Andrii <youdronveduta@gmail.com>
2024-09-22 14:34:42 +0200

    update frontend/src/components/HomePage.jsx

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const HomePage = () => (
+  <div className='col-12 col-md-6 mt-3 mt-md-0'>
+    <h1>Домашнаяя страница</h1>
+    <Link to="/login">Вход</Link>
+  </div>
+);
