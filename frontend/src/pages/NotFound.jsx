f778aae78bc70f8440056d60c037fa7aab01d730
Andrii <youdronveduta@gmail.com>
2024-09-22 12:10:28 +0200

    add frontend/src/pages/NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NotFound = () => (
+  <div className='col-12 col-md-6 mt-3 mt-md-0'>
+    <h1>404 - Страница не найдена</h1>
+    <p>Извините, страница, которую вы ищете, не существует.</p>
+    <Link to="/">Вернуться на главную</Link>
+  </div>
+);
