84738ea25411e95348714f2b7cc79979a1da6268
Andrii <youdronveduta@gmail.com>
2024-09-23 20:02:06 +0200

    update frontend/src/components/HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useFormik } from 'formik';
-import { Button, Form, Container, Card, Row, Col, Nav } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import routes from '../routes.js';
-// import { fetchChannels, selectors } from '../slices/channelsSlice.js';
+import React, { useState } from 'react';
+import { Button, Container, Row, Col, Nav } from 'react-bootstrap';
 
 import { getChannels, addChannel, removeChannel } from '../services/channelsApi.js';
-import { getMessages, addMassage, removeMassage } from '../services/messagesApi.js';
+
+import { MessageBox } from './MessageBox.jsx'
 
 export const HomePage = () => {
   const [activeChannel, setActiveChannel] = useState({ id: '1', name: 'general' });
-  const channels = getChannels();
-  const messages = getMessages();
+  const { data, error, isLoading, refetch } = getChannels();
   const [remove] = removeChannel();
   const [sendTask] = addChannel();
 
   
-  if (channels.isLoading || messages.isLoading) {
+  if (isLoading) {
     return <div>Загрузка</div>;
   }
   const renderChannels = ({ id, name, removable }) => (
@@ -27,47 +23,6 @@ export const HomePage = () => {
       </Button>
     </Nav.Item>
   );
-
-  const renderMassager = () => {
-    const messagesFromChannel =  messages.data.filter(({ channelId }) => channelId === activeChannel.id);
-    const countMessage = messagesFromChannel.length;
-    return (
-      <div className='d-flex flex-column h-100'>
-        <div className='bg-light mb-4 p-3 shadow-sm small'>
-          <p className='m-0'>
-            <b>{`# ${activeChannel.name}`}</b>
-          </p>
-          <span className='text-muted'>
-            {`${countMessage} сообений`}
-          </span>
-        </div>
-        <div id="messages-box" className='chat-messages overflow-auto px-5'>
-          {messagesFromChannel.map(({ id, body, username }) => (
-            <div key={id} className='text-break mb-2'>
-              <b>{username}</b>: {body}
-            </div>
-          ))}
-        </div>
-        <div className='mt-auto px-5 py-3'>
-          <Form noValidate className='py-1 border rounded-2'>
-            <Form.Group className='input-group has-validation'>
-              <Form.Control 
-                type="input"
-                placeholder="Введите сообщение..." 
-                id="message"
-                name="message"
-                className='border-0 p-0 ps-2'
-              />
-              <Button variant='group-vertical' disabled type='submit' >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path></svg>
-                <span className='visually-hidden'>Отправить</span>
-              </Button>
-            </Form.Group>
-          </Form>
-        </div>
-      </div>
-    );
-  };
 
   return (
     <Container className='h-100 my-4 shadow rounded overflow-hidden'>
@@ -84,11 +39,11 @@ export const HomePage = () => {
             </Button>
           </div>
           <Nav as='ul' id='channels-box' className='flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block'>
-            {channels.data.map(renderChannels)}
+            {data.map(renderChannels)}
           </Nav>
         </Col>
         <Col className='p-0 h-100'>
-          {renderMassager()}
+          <MessageBox activeChannel={activeChannel} />
         </Col>
       </Row>
     </Container>
