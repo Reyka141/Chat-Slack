bb67f4b3dc4e055e757f6988f8f7eb59096af0dd
Andrii <youdronveduta@gmail.com>
2024-09-23 14:22:49 +0200

    update frontend/src/index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Component from './components/App';
+import { Provider } from 'react-redux';
+import store from './slices/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Component />);
+root.render(
+  <Provider store={store}>
+    <Component />
+  </Provider>
+);
