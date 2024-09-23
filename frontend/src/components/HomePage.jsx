1006178142397992140b297d3a97ddf30e7dc4bb
Andrii <youdronveduta@gmail.com>
2024-09-23 14:29:57 +0200

    update frontend/src/components/HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import routes from '../routes.js';
 // import { fetchChannels, selectors } from '../slices/channelsSlice.js';
 
-import { getChannels, addChannel, removeChannel } from '../slices/channelsSlice.js';
-import { getMessages, addMassage, removeMassage } from '../slices/messagesApi.js';
+import { getChannels, addChannel, removeChannel } from '../services/channelsApi.js';
+import { getMessages, addMassage, removeMassage } from '../services/messagesApi.js';
 
 export const HomePage = () => {
   const [activeChannel, setActiveChannel] = useState({ id: '1', name: 'general' });
@@ -59,7 +59,7 @@ export const HomePage = () => {
                 className='border-0 p-0 ps-2'
               />
               <Button variant='group-vertical' disabled type='submit' >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path></svg>
                 <span className='visually-hidden'>Отправить</span>
               </Button>
             </Form.Group>
