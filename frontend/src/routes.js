1fd79520dee84aa6855a45d31f6ccca2abd98af6
Andrii <youdronveduta@gmail.com>
2024-09-23 14:23:00 +0200

    update frontend/src/routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,5 +3,5 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
-  usersPath: () => [apiPath, 'data'].join('/'),
+  channelsPath: () => [apiPath, 'channels'].join('/'),
 };
