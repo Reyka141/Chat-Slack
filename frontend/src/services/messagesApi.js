01bd164c74b9b051e54b7e6f699d409bbdbebc8d
Andrii <youdronveduta@gmail.com>
2024-09-23 14:30:17 +0200

    update frontend/src/services/messagesApi.js

---
+++ frontend/src/services/messagesApi.js
@@ -0,0 +1,46 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const messagesApi = createApi({
+  reducerPath: 'messages',
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: '/api/v1/messages',
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('userId'); 
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+   }),
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: () => '',
+    }),
+    addMessage: builder.mutation({
+      query: (text) => ({
+        method: 'POST',
+        body: text,
+      }),
+    }),
+    removeMessage: builder.mutation({
+      query: (id) => ({
+        url: id,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+const {
+  useGetMessagesQuery,
+  useAddMessageMutation,
+  useRemoveMessageMutation,
+} = messagesApi;
+
+export {
+  useGetMessagesQuery as getMessages,
+  useAddMessageMutation as addMessage,
+  useRemoveMessageMutation as removeMessage,
+};
