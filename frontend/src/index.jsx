0fb7f56b3e6a4ecddeb5f6540642e3698c94dcba
Andrii <youdronveduta@gmail.com>
2024-09-26 13:57:48 +0200

    update frontend/src/index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,10 +6,18 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './services/index.js';
 import './i18next.js'
+import { ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: 'd8298ae5f8e948888c1601ffb59e7b5b',
+  environment: 'testenv',
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <ErrorBoundary config={rollbarConfig}>
+      <App />
+    </ErrorBoundary>
   </Provider>
 );
