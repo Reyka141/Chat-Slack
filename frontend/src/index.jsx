957cb2641c90a692e5d25c09da9b8d361e31418a
Andrii <youdronveduta@gmail.com>
2024-09-26 12:00:38 +0200

    update frontend/src/index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './services/index.js';
