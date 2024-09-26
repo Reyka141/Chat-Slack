677ae6649bb90430958b4a13c01c9c38b67fdf98
Andrii <youdronveduta@gmail.com>
2024-09-26 23:53:48 +0200

    update frontend/src/i18next.js

--- frontend/src/i18next.js
+++ frontend/src/i18next.js
@@ -3,12 +3,12 @@ import { initReactI18next } from 'react-i18next';
 import resources from './locales/index.js';
 
 i18next
-  .use(initReactI18next) // передаем экземпляр i18n в react-i18next, который сделает его доступным для всех компонентов через context API.
+  .use(initReactI18next)
   .init({
-    resources, // передаем переводы текстов интерфейса в формате JSON
-    fallbackLng: 'ru', // если переводы на языке пользователя недоступны, то будет использоваться язык, указанный в этом поле
+    resources,
+    fallbackLng: 'ru',
     interpolation: {
-      escapeValue: false, // экранирование уже есть в React, поэтому отключаем
+      escapeValue: false,
     },
   });
 
