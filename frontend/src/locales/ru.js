b4432232659dff98e758d14fc717700cd0b30275
Andrii <youdronveduta@gmail.com>
2024-09-26 17:42:44 +0200

    update frontend/src/locales/ru.js

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -76,7 +76,7 @@ export default {
     },
     toasts: {
       createChannel: 'Канал создан',
-      removeChannel: 'Канал удален',
+      removeChannel: 'Канал удалён',
       renameChannel: 'Канал переименован',
       fetchError: 'Ошибка сети',
       otherError: 'Ошибка в загрузке данных',
