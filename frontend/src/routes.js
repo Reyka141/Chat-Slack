136ec8a9701833bcd9dcecd3f9ad4619ccf5614a
Andrii <youdronveduta@gmail.com>
2024-09-27 12:46:41 +0200

    remove frontend/src/routes.js

--- frontend/src/routes.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-const apiPath = '/api/v1';
-
-export default {
-  loginPath: () => [apiPath, 'login'].join('/'),
-  channelsPath: () => [apiPath, 'channels'].join('/'),
-};
