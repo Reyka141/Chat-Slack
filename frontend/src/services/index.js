3ab1838f1534885499e46e63d7d5c518f6cb2dbe
Andrii <youdronveduta@gmail.com>
2024-09-27 12:48:55 +0200

    update frontend/src/services/index.js

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -3,12 +3,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import { channelsApi } from './channelsApi.js';
 import { messagesApi } from './messagesApi.js';
 import { userApi } from './userApi.js';
+import channelsReduser from './channelsSlice.js';
+import messagesReducer from './messagesSlice.js';
 
 const store = configureStore({
   reducer: {
     [channelsApi.reducerPath]: channelsApi.reducer,
     [messagesApi.reducerPath]: messagesApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
+    channelsSlice: channelsReduser,
+    messagesSlice: messagesReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(channelsApi.middleware, messagesApi.middleware, userApi.middleware),
