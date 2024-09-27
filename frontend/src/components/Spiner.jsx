78bfa7ba9ffcfda19cca43f393b7cddb921c0e04
Andrii <youdronveduta@gmail.com>
2024-09-27 12:47:42 +0200

    add frontend/src/components/Spiner.jsx

---
+++ frontend/src/components/Spiner.jsx
@@ -0,0 +1,17 @@
+import { Container, Row, Spinner} from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+
+const Spiner = () => {
+  const { t } = useTranslation();
+  return (
+    <Container fluid className="h-100">
+      <Row className="justify-content-center align-content-center h-100">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">{t('homePage.loading')}</span>
+        </Spinner>
+      </Row>
+    </Container>
+  );
+}
+
+export default Spiner;
