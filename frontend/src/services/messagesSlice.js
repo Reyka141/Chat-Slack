c590f81e73dcb4684b41207f8a03079c6de2d61d
Andrii <youdronveduta@gmail.com>
2024-09-27 12:49:12 +0200

    add frontend/src/services/messagesSlice.js

---
+++ frontend/src/services/messagesSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import { actions as channelsActions } from './channelsSlice';
+
+const messagesAdapter = createEntityAdapter();
+
+const initialState = messagesAdapter.getInitialState();
+
+const messagesSlice = createSlice({
+  name: 'messagesSlice',
+  initialState,
+  reducers: {
+    addMessages: messagesAdapter.addMany,
+    addMessage: messagesAdapter.addOne,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(channelsActions.removeChannel, (state, action) => {
+      const { id } = action.payload;
+      const restEntities = Object.values(state.entities).filter((e) => e.channelId !== id);
+      messagesAdapter.setAll(state, restEntities);
+    })
+  }
+});
+
+export const { actions } = messagesSlice;
+export const selector = messagesAdapter.getSelectors((state) => state.messagesSlice);
+export default messagesSlice.reducer;
